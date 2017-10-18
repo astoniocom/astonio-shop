a5c816f3541dc0c101db23aa3c318eaac5032e96
Viktar Katlianik <7532425@gmail.com>
2017-10-18 17:00:57 +0300

    Database structure changed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,14 +17,14 @@ import { AppComponent } from './app.component';
 import ssh2 from 'ssh2';
 
 var config = {
-  ssh: {
+  /*ssh: {
     host: '',
     username: '',
     password: '',
     port:undefined,
     keepaliveInterval: 4000,
     readyTimeout: 5000,
-  },
+  },*/
   mysql: {
     host: 'localhost',
     user: '',
